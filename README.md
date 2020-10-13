d6cf9b6e66bfa4a1d21c7b0da0c20d5eca6c1794
Joword <23089538@qq.com>
2020-10-13 10:08:49 +0800

    1. UPDATE the last version of HandleDatabase.py 2. UPDATE the README.md

--- README.md
+++ README.md
@@ -10,8 +10,9 @@ to merge two tables with the same index
 db
 >the function of file folder was connected database by mysql.connector.<br>
 Besides, it has been sshtunel to connect your database at all.<br>
-**The DataBase.py was the tool of connecting your wanted database.**<br>
-Anyway, I want to enrich database connection in future.
+**The DatabaseAccess.py was the tool of connecting your wanted database.**<br>
+Anyway, I want to enrich database connection in future.<br>
+**The HandleDatabase.py was a tool of handle the database by SSH connecting**<br>
 
 analysis
 >it was the folder of handling the data from the folder of db.<br>
--- db/HandleDatabase.py
+++ db/HandleDatabase.py
@@ -2,6 +2,7 @@
 #@Time : 2020/10/10 0010 10:14
 #@Author: Joword
 #@File : HandleDatabase.py
+#@Update : Joword 2020/10/13 10:06
 
 import re
 import datetime
@@ -23,8 +24,30 @@ class HandleDatabase(DatabaseAccess):
 	def get_header(self, table_description:list)->list:
 		return [i[0] for i in table_description]
 	
+	def make_data(self, input_data:dict)->tuple:
+		u'''主要做数据修改
+		:param input_data: SELECT得到的数据
+		:return: 元组(未修改的字典型列表，已修改的字典型列表)
+		'''
+		label_list,output_data = list(),list()
+		for content in input_data['data']:
+			if content[10] is 2:
+				label_dict = dict()
+				label_dict[content[2]] = content[1]
+				label_dict['criteria'] = content[8]
+				label_dict['header'] = input_data['header']
+				label_list.append(label_dict)
+			content[0] = None
+			content[1] = int(42)
+			content[-4] = datetime.datetime.now()
+			content[-2] = datetime.datetime.now()
+			content = tuple(content)
+			output_data.append(content)
+		return label_list,output_data
+	
 	def database_tables_name(self, input_data:dict, table_name:str):
-		u'''功用：根据criteria匹配数据库中pm3bp2,pp1bs4,pp4bp5,ps2pm6,ps3bs3,ps4，并返回是否匹配上
+		u'''功用：根据criteria匹配数据库中pm3bp2,pp1bs4,pp4bp5,ps2pm6,ps3bs3,ps4，并返回是否匹配上，将criteria做匹配放到对应表
+		名的字典字段中，同时也对数据库中表明做匹配放到数据库对应表名_match字段中
 		:param table_name: 数据库中所需要匹配的表名
 		:param input_data:输入数据
 		:return:True/False
@@ -51,9 +74,10 @@ class HandleDatabase(DatabaseAccess):
 		return input_data
 		
 	def select_database_data(self, table_name:str, conditions=None) -> dict:
-		u'''连接数据库，做sql操作后拿到数据
+		u'''连接数据库，做sql操作后SELECT数据，经测试不可直接对SELECT数据操作，遂需分两步进行，并且为保证数据完整性，需要双循环
+		
 		:param table_name: sql表名字
-		:param conditions: 输入
+		:param conditions: 输入条件
 		:return:
 		'''
 		with SSHTunnelForwarder(("192.168.29.37", 22), ssh_username='vardecoder', ssh_password='VarDecoder', remote_bind_address=('0.0.0.0', 3306)) as server:
@@ -103,6 +127,12 @@ class HandleDatabase(DatabaseAccess):
 				return {"header":table_header, "data":results}
 		
 	def insert_to_tables(self, table_name:str, data:dict):
+		# TODO:亮点是对需要单个写入的数据，进行批量写入
+		u'''插入数据，在对拿到数据做处理后
+		:param table_name: 数据库中表名
+		:param data: 字典型数据
+		:return: 无返回，需要try/except/finally关闭数据库否则会一直运行
+		'''
 		with SSHTunnelForwarder(("192.168.29.37",22), ssh_username='vardecoder', ssh_password='VarDecoder',remote_bind_address=('0.0.0.0',3306)) as server:
 			try:
 				connect = mc.connect(host="127.0.0.1", port=server.local_bind_port, user=self.user, passwd=self.passwd, database=self.database)
@@ -119,32 +149,21 @@ class HandleDatabase(DatabaseAccess):
 			finally:
 				connect.close()
 	
-	def make_data(self):
-		inputData,label_list,pm3bp2_data,pp1bs4_data,pp4bp5_data,ps2pm6_data,ps3bs3_data,ps4_data,user_evidence = list(),list(),list(),list(),list(),list(),list(),list(),list()
-		#数据获取
+	def main(self):
+		u'''主程序入口，
+		:return:
+		'''
+		# index数据获取
 		results = self.select_database_data("user_collect",self.get_index("50 discrepant variants.xlsx"))
 		# 数据处理
-		for content in results['data']:
-			if content[10] is 2:
-				label_dict = dict()
-				label_dict[content[2]] = content[1]
-				label_dict['criteria'] = content[8]
-				label_dict['header'] = results['header']
-				label_list.append(label_dict)
-			content[0] = None
-			content[1] = int(42)
-			content[-4] = datetime.datetime.now()
-			content[-2] = datetime.datetime.now()
-			content = tuple(content)
-			inputData.append(content)
-		# pm3bp2 = self.select_database_data("user_pm3bp2",label_list)
-		# pp1bs4 = self.select_database_data("user_pp1bs4",label_list)
-		# pp4bp5 = self.select_database_data("user_pp4bp5",label_list)
-		# ps3bs3 = self.select_database_data("user_ps3bs3", label_list)
-		
-		self.insert_to_tables(table_name="test", data=pm3bp2)
-		
-if __name__ == '__main__':
-	test = HandleDatabase(host='192.168.29.37',user='vardecoder',passwd='Decoder#123',database='varDecoding')
-	test.make_data()
+		data_new = self.make_data(results)
+		# 数据修改
+		for table in ['user_pm3bp2','user_pp1bs4','user_pp4bp5','user_ps2pm6','user_ps3bs3','user_ps4']:
+			datas = self.select_database_data(table,data_new[0])
+			if len(datas['data']) > 0:
+				self.insert_to_tables(table_name="test",data=datas)
+
+# if __name__ == '__main__':
+# 	test = HandleDatabase(host='192.168.29.37',user='vardecoder',passwd='Decoder#123',database='varDecoding')
+# 	test.main()
 	
